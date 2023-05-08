ec5f5b2deb028144a4a271ff12947fb029e95e32
AlaPedro <alanpedro94@gmail.com>
2023-05-08 18:10:55 -0300

    feat: height nav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -38,17 +38,17 @@ export default function Header() {
 
                 <div className="absolute top-0 left-0 w-full h-full bg-black/80">
                     <div className="flex flex-col h-screen justify-center gap-10 items-center">
-                        <Link href={'/infos'} className="flex items-center justify-center text-white font-semibold bg-orange-500 w-4/5 h-10 rounded-md hover:scale-105 ease-in-out duration-500 hover:bg-orange-500">
+                        <Link href={'/infos'} className="flex items-center justify-center text-white font-semibold bg-orange-500 w-4/5 h-12 rounded-md hover:scale-105 ease-in-out duration-500 hover:bg-orange-500">
                             <button >
                                 O que é taxa metábolica basal (TMB) ?
                             </button>
                         </Link>
 
-                        <Link href={"infos/manipulando-peso"} className="flex items-center justify-center text-white font-semibold bg-orange-500 w-4/5 h-10 rounded-md hover:scale-105 ease-in-out duration-500 hover:bg-orange-500">
+                        <Link href={"infos/manipulando-peso"} className="flex items-center justify-center text-white font-semibold bg-orange-500 w-4/5 h-12 rounded-md hover:scale-105 ease-in-out duration-500 hover:bg-orange-500">
                             <button >Como perder/ganhar peso sabendo a (TMB) ?</button>
                         </Link>
 
-                        <button className="text-white font-semibold bg-orange-500 w-4/5 h-10 rounded-md hover:scale-105 ease-in-out duration-500 hover:bg-orange-500" onClick={handleCloseMenu}>Voltar</button>
+                        <button className="text-white font-semibold bg-orange-500 w-4/5 h-12 rounded-md hover:scale-105 ease-in-out duration-500 hover:bg-orange-500" onClick={handleCloseMenu}>Voltar</button>
                     </div>
                 </div>
 
